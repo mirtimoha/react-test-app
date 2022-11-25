3b2693810cd0380b33c96e566440255c6ecea61c
mirtimoha <44141858+mirtimoha@users.noreply.github.com>
2022-11-25 17:57:52 +0500

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const subject = "React";
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start edi ting to see some magic happen :)</p>
+      <p>Start edi ting to see some m agic happen {subject}:)</p>
     </div>
   );
 }
